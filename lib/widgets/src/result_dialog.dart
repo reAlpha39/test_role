5dae439b3486be10fe875982a78e62fb4052f4b1
reAlpha39 <yunusw718@gmail.com>
2024-11-06 13:14:18 +0700

    fix: font error on result dialog

--- lib/widgets/src/result_dialog.dart
+++ lib/widgets/src/result_dialog.dart
@@ -14,22 +14,145 @@ class ResultDialog {
       useSafeArea: false,
       context: context,
       barrierDismissible: true,
-      builder: (context) => Center(
-        child: Stack(
-          alignment: AlignmentDirectional.topCenter,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 36,
-                left: 16,
-                right: 16,
-                bottom: 16,
-              ),
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                constraints: const BoxConstraints(
-                  maxWidth: 500,
+      builder: (context) => Material(
+        child: Center(
+          child: Stack(
+            alignment: AlignmentDirectional.topCenter,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 36,
+                  left: 16,
+                  right: 16,
+                  bottom: 16,
                 ),
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  constraints: const BoxConstraints(
+                    maxWidth: 500,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xff3E4095),
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(
+                      color: const Color(0xFFFFB819),
+                      width: 10,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(height: 32),
+                      if (id == 1) ...[
+                        RepaintBoundary(
+                          key: shareCubit.globalKey,
+                          child: Stack(
+                            alignment: AlignmentDirectional.topCenter,
+                            children: [
+                              Image.asset(
+                                'assets/images/hipster_card.png',
+                                width: 400,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 24),
+                                child: Text(
+                                  'Congratulations!!\n${cubit.name}',
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.bubblegumSans(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                      if (id == 2) ...[
+                        RepaintBoundary(
+                          key: shareCubit.globalKey,
+                          child: Stack(
+                            alignment: AlignmentDirectional.topCenter,
+                            children: [
+                              Image.asset(
+                                'assets/images/hustler_card.png',
+                                width: 400,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 24),
+                                child: Text(
+                                  'Congratulations!!\n${cubit.name}',
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.bubblegumSans(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                      if (id == 3) ...[
+                        RepaintBoundary(
+                          key: shareCubit.globalKey,
+                          child: Stack(
+                            alignment: AlignmentDirectional.topCenter,
+                            children: [
+                              Image.asset(
+                                'assets/images/hacker_card.png',
+                                width: 400,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 24),
+                                child: Text(
+                                  'Congratulations!!\n${cubit.name}',
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.bubblegumSans(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                      const SizedBox(height: 24),
+                      GestureDetector(
+                        onTap: () async {
+                          await shareCubit.downloadImage();
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 8,
+                            horizontal: 32,
+                          ),
+                          decoration: BoxDecoration(
+                            color: const Color(0xff3E4095),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: const Color(0xFFFFB819),
+                              width: 10,
+                            ),
+                          ),
+                          child: const Text(
+                            'Download',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
                 decoration: BoxDecoration(
                   color: const Color(0xff3E4095),
                   borderRadius: BorderRadius.circular(30),
@@ -38,138 +161,18 @@ class ResultDialog {
                     width: 10,
                   ),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(height: 32),
-                    if (id == 1) ...[
-                      RepaintBoundary(
-                        key: shareCubit.globalKey,
-                        child: Stack(
-                          alignment: AlignmentDirectional.topCenter,
-                          children: [
-                            Image.asset(
-                              'assets/images/hipster_card.png',
-                              width: 400,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24),
-                              child: Text(
-                                'Congratulations!!\n${cubit.name}',
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.bubblegumSans(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                    if (id == 2) ...[
-                      RepaintBoundary(
-                        key: shareCubit.globalKey,
-                        child: Stack(
-                          alignment: AlignmentDirectional.topCenter,
-                          children: [
-                            Image.asset(
-                              'assets/images/hustler_card.png',
-                              width: 400,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24),
-                              child: Text(
-                                'Congratulations!!\n${cubit.name}',
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.bubblegumSans(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                    if (id == 3) ...[
-                      RepaintBoundary(
-                        key: shareCubit.globalKey,
-                        child: Stack(
-                          alignment: AlignmentDirectional.topCenter,
-                          children: [
-                            Image.asset(
-                              'assets/images/hacker_card.png',
-                              width: 400,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24),
-                              child: Text(
-                                'Congratulations!!\n${cubit.name}',
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.bubblegumSans(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                    const SizedBox(height: 24),
-                    GestureDetector(
-                      onTap: () async {
-                        await shareCubit.downloadImage();
-                        Navigator.of(context).pop();
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 8,
-                          horizontal: 32,
-                        ),
-                        decoration: BoxDecoration(
-                          color: const Color(0xff3E4095),
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            color: const Color(0xFFFFB819),
-                            width: 10,
-                          ),
-                        ),
-                        child: const Text(
-                          'Download',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
+                child: const Text(
+                  'Hasil',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-              decoration: BoxDecoration(
-                color: const Color(0xff3E4095),
-                borderRadius: BorderRadius.circular(30),
-                border: Border.all(
-                  color: const Color(0xFFFFB819),
-                  width: 10,
-                ),
-              ),
-              child: const Text(
-                'Hasil',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
